d28eebdc3b4045cb67c88fb7cbf5c51725e31b2d
uriel <uriel@URIEL>
2026-06-25 21:28:13 -0600

    cambio de la sintesis de la linea 85

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -135,7 +135,7 @@ export default function Purchases() {
                     </td>
                   </tr>
                 )}
-              </>
+              </React.Fragment>
             ))}
           </tbody>
         </table>
